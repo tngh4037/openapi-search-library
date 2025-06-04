a2867d63f58a4018e28145eae63b96970b436bd9
tngh4037 <tngh4037@gmail.com>
2025-06-05 04:41:43 +0900

    feat: 프로젝트 세팅

---
+++ src/test/java/com/library/library_search/LibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.library_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
