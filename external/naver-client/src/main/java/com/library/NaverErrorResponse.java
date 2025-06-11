a082dbbfdf4a4fcb135dbc0cd62fe6f22945eb7e
tngh4037 <tngh4037@gmail.com>
2025-06-11 18:06:14 +0900

    [LS-4] 외부 api 연동 - 역직렬화를 위해 NaverErrorResponse 기본생성자 추가

--- external/naver-client/src/main/java/com/library/NaverErrorResponse.java
+++ external/naver-client/src/main/java/com/library/NaverErrorResponse.java
@@ -1,5 +1,13 @@
 package com.library;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NaverErrorResponse {
     private String errorMessage;
     private String errorCode;
@@ -7,13 +15,5 @@ public class NaverErrorResponse {
     public NaverErrorResponse(String errorMessage, String errorCode) {
         this.errorMessage = errorMessage;
         this.errorCode = errorCode;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
     }
 }
